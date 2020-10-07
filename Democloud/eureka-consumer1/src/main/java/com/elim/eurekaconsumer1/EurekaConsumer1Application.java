78260cfc3c4ba1cc26a3b32e46b4b31900739792
elim <15234230405@163.com>
2020-10-08 03:45:20 +0800

    Test eureka-cluster

---
+++ Democloud/eureka-consumer1/src/main/java/com/elim/eurekaconsumer1/EurekaConsumer1Application.java
@@ -0,0 +1,13 @@
+package com.elim.eurekaconsumer1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaConsumer1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaConsumer1Application.class, args);
+    }
+
+}
