e3e9300dc93c607a072a14d97759a3d9762867e0
elim <15234230405@163.com>
2020-10-08 03:02:13 +0800

    eureka-server&eureka-client -provider&-consumer

---
+++ eureka-server/src/test/java/com/elim/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elim.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
